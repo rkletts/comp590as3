92a22ebe96f6c85e8aba5dcf4305f6804c447e04
Maria Thomas <111470032+mlthoma@users.noreply.github.com>
2025-02-12 20:30:28 -0500

    Update p2.erl

--- p2.erl
+++ p2.erl
@@ -1,33 +1,38 @@
--module(number_processor).
--export([start/0, process_number/1, factorial/1]).
-
+-module(p2).
+-export([start/0, factorial/1]).
 
 start() -> 
     loop().
 
 loop() -> 
-    {ok, Num} = io:read("Enter a number: "),
-    if
-        not is_integer(Num) -> 
-            io:format("not an integer~n"),
-            loop();
-        Num < 0 -> 
-            Result = math:pow(abs(Num), 7),
-            io:format("~w raised to the 7th power is: ~w~n", [abs(Num), Result]),
-            loop();
-        Num == 0 -> 
-            io:format("0~n");
-        Num > 0 -> 
+    io:format("Enter a number: "),
+    {ok, Num} = io:fread("", "~d"),  % Reads an integer
+    case Num of
+        eof -> io:format("Goodbye!~n");  % Handles end of input
+        [Value] -> 
             if
-                Num rem 7 == 0 -> 
-                    Root = math:pow(Num, 1/5),
-                    io:format("The 5th root of ~w is: ~w~n", [Num, Root]),
+                Value < 0 -> 
+                    Result = math:pow(abs(Value), 7),
+                    io:format("~w raised to the 7th power is: ~w~n", [abs(Value), Result]),
                     loop();
-                true -> 
-                    Fact = factorial(Num),
-                    io:format("The factorial of ~w is: ~w~n", [Num, Fact]),
-                    loop()
-            end
+                Value == 0 -> 
+                    io:format("0~n"),
+                    loop();
+                Value > 0 -> 
+                    if
+                        Value rem 7 == 0 -> 
+                            Root = math:pow(Value, 1/5),
+                            io:format("The 5th root of ~w is: ~w~n", [Value, Root]),
+                            loop();
+                        true -> 
+                            Fact = factorial(Value),
+                            io:format("The factorial of ~w is: ~w~n", [Value, Fact]),
+                            loop()
+                    end
+            end;
+        _ -> 
+            io:format("Invalid input. Please enter an integer.~n"),
+            loop()
     end.
 
 factorial(0) -> 1;
