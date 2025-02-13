6fa885a2d339a485087a45e4eaf13e290cd21b1d
Maria Thomas <111470032+mlthoma@users.noreply.github.com>
2025-02-12 20:12:55 -0500

    Update p2.erl

--- p2.erl
+++ p2.erl
@@ -1,30 +1,30 @@
 -module(number_processor).
 -export([start/0, process_number/1, factorial/1]).
 
--team("reese letts, maria thomas")
+-team("reese letts, maria thomas").
 
-start() ->
+start() -> 
     loop().
 
-loop() ->
+loop() -> 
     {ok, Num} = io:read("Enter a number: "),
     if
-        not is_integer(Num) ->
+        not is_integer(Num) -> 
             io:format("not an integer~n"),
             loop();
         Num < 0 -> 
             Result = math:pow(abs(Num), 7),
             io:format("~w raised to the 7th power is: ~w~n", [abs(Num), Result]),
             loop();
-        Num == 0 ->
+        Num == 0 -> 
             io:format("0~n");
-        Num > 0 ->
+        Num > 0 -> 
             if
-                Num rem 7 == 0 ->
+                Num rem 7 == 0 -> 
                     Root = math:pow(Num, 1/5),
                     io:format("The 5th root of ~w is: ~w~n", [Num, Root]),
                     loop();
-                true ->
+                true -> 
                     Fact = factorial(Num),
                     io:format("The factorial of ~w is: ~w~n", [Num, Fact]),
                     loop()
